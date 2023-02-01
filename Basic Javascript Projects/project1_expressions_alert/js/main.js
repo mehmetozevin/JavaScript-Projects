55dadf00cb8f6d45bf3a4eb377a4d65efe412572
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-02-01 07:36:44 +0000

    Add files via upload

---
+++ Basic Javascript Projects/project1_expressions_alert/js/main.js
@@ -0,0 +1,56 @@
+window.alert("Window Alert sample, Hello, World!"); // window.alert sample is here
+
+document.write("Document write sample"); // doc write sample is here
+
+/* 
+
+var a = "This is a string sample";
+
+var a = "Ayse said \"This course is getting more exciting\" "; //quote mark is given
+
+var b = "ayse durumun daha heyecanli hale geldigini soyledi"
+
+var c = a + b
+
+var d = "bu da d degiskeni"
+
+document.write = fontcolor((c + d));
+
+=======
+var a="This is a string sample";
+
+var a = "Ayse said \"This course is getting more exciting\" ";
+
+var b = "ayse durumun daha heyecanli hale geldigini soyledi"
+
+var c = a + b
+
+var d = "bu da d degiskeni"
+
+document.write= fontcolor((c + d));
+==================================
+=================================
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues)
+============================
+===========================
+Assignment 12, write an expression:
+
+
+var a = 3;
+var b = 4;
+
+var c = (a + b)
+
+document.write(c)
+
+=====
+var placesLived = january = "Canon Beck Road", february = "Swan Road",
+    march = "churchill road", april = "highgate road", may = "crouchend", june = "jackson crescent", july = "colin murphy road"
+august = "darkfield lane", september = "grantham court";
+
+document.write(september);
+
+*/
+
