fd1060e9c19c8312d55e5eacbc4bff154d646e9c
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-02-02 09:24:24 +0000

    Add files via upload

---
+++ Basic Javascript Projects/project2_functions/js/main.js
@@ -0,0 +1,21 @@
+/* ======================================
+Assignment 13
+
+
+function changeText() {
+    var x = "Hello";
+    var y = "World";
+    document.getElementById("text").innerHTML = x + " " + y;
+
+}
+*/
+
+/* ======================================
+Assignment 14 Using +=
+===============================*/
+
+function myFunction() {
+    var x = "3 worlds will be added with += here: ";
+    x += " Wrold World World";
+    document.getElementById("concatenate").innerHTML = x;
+}
