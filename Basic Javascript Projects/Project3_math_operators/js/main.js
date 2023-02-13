85f881c968b25fceddeaad6779d5290bcde45ed4
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-02-13 11:22:59 +0000

    Add files via upload

---
+++ Basic Javascript Projects/Project3_math_operators/js/main.js
@@ -0,0 +1,70 @@
+/* 
+Assignment 16, create and call a function 
+function addNumbers(a, b) {
+    document.getElementById("Math").innerHTML = a + b;
+ }
+ END OF ASSIGNMENT 16
+*/
+
+
+/*
+function subtraction(a, b) {
+    document.getElementById("Math").innerHTML = a - b;
+}
+*/
+
+
+/* Assignment 18, division and multiply
+
+function multiply(a, b) {
+    document.getElementById("Math").innerHTML = a * b;
+}
+
+
+function division(a, b) {
+    document.getElementById("Math").innerHTML = a / b;
+}
+
+END OF ASSIGNMENT 18*/
+
+/*
+
+Assignment 19, Modulus Operator 
+function modulus_sample(a, b) {
+    document.getElementById("Math").innerHTML = a % b;
+}
+
+END OF ASSIGNMENT 19*/
+
+/* Assignment 20, Modulus Operator 
+
+function negation_sample(a) {
+    document.getElementById("Math").innerHTML = -a;
+}
+
+END OF ASSIGNMENT 20*/
+
+/* Assignment 22, Increment and decrement
+let x = 6
+x++;
+document.write(x);
+
+let x = 10
+x--;
+document.write(x);
+
+*/
+
+/* Assignemtn 22, math Random
+document.write(Math.random() * 100);
+
+*/
+
+/* Challange 4 Learning Math and Math object
+
+document.write(Math.floor(4.87686));
+
+let x = Math.floor(Math.random() * 10);
+document.write(x);
+END of Challenge 4
+*/
