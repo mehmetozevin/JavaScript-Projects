6d6901b7d48bcdca99866be355cab8a8f646591a
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-02-13 13:56:35 +0000

    Add files via upload

---
+++ Basic Javascript Projects/project4_dictionaries/js/main.js
@@ -0,0 +1,32 @@
+/* Assignemt 24, Dictionary
+
+function my_dictionary () {
+    var animal= {
+        name_ :"Kermit",
+        species :"Cat",
+        age : 2,
+        breed:"Norwegian Forest Cat",
+        appearance:"Blue eyes, very fluffy",
+    };
+
+    document.getElementById("dictionaries").innerHTML= animal.age ;
+
+}
+*/
+
+/*
+Assignment 25, deleting dictionary
+
+function my_dictionary () {
+    var animal= {
+        name_ :"Kermit",
+        species :"Cat",
+        age : 2,
+        breed:"Norwegian Forest Cat",
+        appearance:"Blue eyes, very fluffy",
+    };
+    delete animal.age;
+    document.getElementById("dictionaries").innerHTML= animal.age ;
+}
+
+*/
